7198272be6cf2aebb31b21aee34ff5b2d1fc1f08
alexandre1Work <alexandre.velosowork@gmail.com>
2025-02-05 17:29:40 -0300

    feat

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -6,5 +17,17 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+    let despesa = new Despesa(
+        ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value
+    );
+
+    gravar(despesa);
 }
+
+function gravar(d) {
+    //1p - identificação do dado que queremos armazenar
+    //2p - o proprio dado que queremos armazenar - PRECISA ser em formato JSON
+    //possui id - identificador único, o que quer dizer que uma despesa atribuida ao mesmo ide sobrepoe ele
+    localStorage.setItem('despesa', JSON.stringify(d));
+}
+
