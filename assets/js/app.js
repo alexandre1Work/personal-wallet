01f71acba6672f013ce4e3712c879925ee8dbcef
alexandre1Work <alexandre.velosowork@gmail.com>
2025-02-10 15:43:19 -0300

    validating data before registration

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,16 @@ class Despesa {
         this.descricao = descricao;
         this.valor = valor;
     }
+
+    validarDados() {
+        //for ... in percorre todas as chaves de um objeto
+        for (let i in this){
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            } 
+        }
+        return true
+    }
 }
 
 class BD {
@@ -58,9 +68,62 @@ function cadastrarDespesa() {
         ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value
     );
 
-    //chama a classe para gravar em localStorage
-    bd.gravar(despesa); //envia um objeto
+    let modalTitulo = document.getElementById('modalTitulo');
+    let modalTexto = document.getElementById('modalTexto');
+
+    if (despesa.validarDados()) {
+        //sucesso
+        //chama a classe para gravar em localStorage
+        bd.gravar(despesa); //envia um objeto
+
+        //mensagem
+        modalTitulo.innerHTML = "SUCESSO! ✅";
+        modalTexto.innerHTML = "Seus dados foram salvos com sucesso";
+        modalTitulo.style.color = "green";
+        voltarCorrigir.style.display = "none"; // Esconde o botão "Voltar e corrigir"
+        continuar.style.display = "block"; // Exibe o botão "Continuar"
+        continuar.style.backgroundColor = "green";
+    } else {
+        //erro
+        modalTitulo.innerHTML = "ERRO! ❌";
+        modalTexto.innerHTML = "Verifique se todos os campos foram preenchidos corretamente";
+        modalTitulo.style.color = "red";  
+        voltarCorrigir.style.display = "block"; // Esconde o botão "Voltar e corrigir"
+        continuar.style.display = "none"; // Exibe o botão "Continuar"
+        voltarCorrigir.style.backgroundColor = "red";
+    }
+
+    modal.style.display = "block"; // Exibe o modal
+
 }
 
+//configurando modal
+let modal = document.getElementById('meuModal');
+let btn = document.getElementById('abrirModal');
+let span = document.getElementsByClassName('fechar')[0];
+let voltarCorrigir = document.getElementById('voltarCorrigir');
+let continuar = document.getElementById('continuar');
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+voltarCorrigir.onclick = function() {
+    modal.style.display = "none";
+}
+
+continuar.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
+btn.onclick = function() {
+    cadastrarDespesa();
+}
 
 
